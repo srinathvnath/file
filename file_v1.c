56acd7365391903aa7a5bb41b92c5fb062ed9d7a
srinathvnath <32021833+srinathvnath@users.noreply.github.com>
2019-12-06 22:04:32 +0530

    Update file_v1.c

--- file_v1.c
+++ file_v1.c
@@ -1,15 +1,25 @@
-#include <stdio.h>
-
-int i;
-int main()
-{
-    int s;
-    scanf("%d",&s);
-    for(i=1;i<=s;i++)
-    {
-        f=f*i;
-    }
-    printf("Factorial of number %d is %d",s,f);
-    return 0;
+#include<stdio.h>
+void sumpro(int,int,int*,int*);
+int main() {
+  int n=10;
+  int arr[10];
+  fill(arr,n);
+  sumarr(arr,n);
+  display(arr,n);
+  
+  return 0;
 }
-
+void fill(int* parr,int n) {
+  for(int i=0;i<n;i++)
+    parr[i]=i*10+1;  //scanf("%d",&arr[i]);
+    //*parr++=rand()%10;  
+}
+void sumarr(const int* parr, int n) {
+  int sum=0;
+  for(int i=0;i<n;i++)
+    sum+=*parr++;   //sum+=parr[i]  //sum+=*(parr+i)
+}
+void display(const int *parr,int n) {
+  for(int i=0;i<n;i++)
+    printf("%d\n",parr[i]);  //*parr++, *(parr+i)
+}
